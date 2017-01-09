84cea28c1f476ca0706065af055f09dacde1fea9
tranminhtri <tran.minh.tri@framgia.com>
2017-01-09 11:24:50 +0700

    init

---
+++ app/src/main/java/com/framgia/rss_6/data/model/NewsModel.java
@@ -0,0 +1,8 @@
+package com.framgia.rss_6.data.model;
+
+/**
+ * Created by tri on 09/01/2017.
+ */
+public class NewsModel {
+
+}
---
+++ app/src/main/java/com/framgia/rss_6/ui/MainActivity.java
@@ -0,0 +1,14 @@
+package com.framgia.rss_6.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.framgia.rss_6.R;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
